877fed9903e53440c32cb9cd11cb6306edf0bbc6
mostafij <mostafij.programmer@gmail.com>
2015-03-01 07:46:26 +0600

    Spring Practices
    It’s initializing Collection

---
+++ src/org/mostafij/javaspring/DrowingApp.java
@@ -0,0 +1,19 @@
+package org.mostafij.javaspring;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+public class DrowingApp {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle)context.getBean("triangle");
+		
+		triangle.draw();
+
+	}
+
+}
